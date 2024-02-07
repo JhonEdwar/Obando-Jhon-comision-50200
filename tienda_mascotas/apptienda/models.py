899ad395a281fe57837d9f1362884f56e713a155
Jhon Edwar <jhonedwar192@gmail.com>
2024-02-06 23:14:13 -0500

    agregar el administrador

--- tienda_mascotas/apptienda/models.py
+++ tienda_mascotas/apptienda/models.py
@@ -9,15 +9,26 @@ class Cliente(models.Model):
     email = models.EmailField()
     ultima_compra = models.DateField()
 
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+
 class Mascota(models.Model):
     tipo = models.CharField(max_length=50)
     raza = models.CharField(max_length=50)
     nombre = models.CharField(max_length=50)
     edad= models.IntegerField()
+
+    def __str__(self):
+        return f"{self.tipo} de nombre {self.nombre}"
+
    
 class Producto(models.Model):
     nombre_producto = models.CharField(max_length=50)
     categoria = models.CharField(max_length=50)
     tipo_de_animal = models.CharField(max_length=50)
     disponibilidad= models.BooleanField()
+
+    def __str__(self):
+        return f"{self.nombre_producto}"
     
